d8d797a78c6a2f420faa3cc0328df7d408e8b57e
Ria Garg <ria.garg@razorpay.com>
2017-03-21 12:30:19 +0530

    Orders Api Integration (#1)
    * Orders Api Integration
    
    * added 1.2.0 version

--- src/main/java/com/razorpay/PaymentResource.java
+++ src/main/java/com/razorpay/PaymentResource.java
@@ -27,8 +27,14 @@ public class PaymentResource {
 
   @GET
   @Produces(MediaType.TEXT_HTML)
-  public View getPaymentForm() {
-    return new PaymentView(amount);
+  public View getPaymentForm() throws RazorpayException {
+    JSONObject options = new JSONObject();
+    options.put("amount", amount); // Note: The amount should be in paise.
+    options.put("currency", "INR");
+    options.put("receipt", "txn_123456");
+    options.put("payment_capture", 1);
+    Order order = client.Orders.create(options);
+    return new PaymentView(amount, (String) order.get("id"));
   }
 
   @POST
@@ -37,13 +43,21 @@ public class PaymentResource {
   @Produces(MediaType.APPLICATION_JSON)
   public Response charge(MultivaluedMap<String, String> formParams) {
     String paymentId = formParams.getFirst("razorpay_payment_id");
+    String razorpaySignature = formParams.getFirst("razorpay_signature");
+    String orderId = formParams.getFirst("razorpay_order_id");
     JSONObject options = new JSONObject();
-    options.put("amount", amount);
 
-    if (StringUtils.isNotBlank(paymentId)) {
+    if (StringUtils.isNotBlank(paymentId) && StringUtils.isNotBlank(razorpaySignature)
+        && StringUtils.isNotBlank(orderId)) {
       try {
-        Payment payment = client.Payments.capture(paymentId, options);
-        return Response.ok(payment.toString()).build();
+        options.put("razorpay_payment_id", paymentId);
+        options.put("razorpay_order_id", orderId);
+        options.put("razorpay_signature", razorpaySignature);
+        boolean isEqual = client.Utility.verifyPaymentSignature(options);
+
+        if (isEqual) {
+          return Response.ok().build();
+        }
       } catch (RazorpayException e) {
         System.out.println("Exception caused because of " + e.getMessage());
         return Response.status(Status.BAD_REQUEST).build();
--- src/main/java/com/razorpay/PaymentView.java
+++ src/main/java/com/razorpay/PaymentView.java
@@ -5,13 +5,19 @@ import io.dropwizard.views.View;
 public class PaymentView extends View {
 
   private int amount;
+  private String razorpayOrderId;
 
-  public PaymentView(int amount) {
+  public PaymentView(int amount, String razorpayOrderId) {
     super("/index.ftl");
     this.amount = amount;
+    this.razorpayOrderId = razorpayOrderId;
   }
 
   public int getAmount() {
     return amount;
+  }
+
+  public String getRazorpayOrderId() {
+    return razorpayOrderId;
   }
 }
